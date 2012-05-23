9627a8fa0fb62862a02d4f2b9032ea813e043206
Rafael Fribla Castro <rfribla@gmail.com>
2012-05-23 02:29:52 -0400

    Integracion basica Facebook

--- application/controllers/IntegracionController.php
+++ application/controllers/IntegracionController.php
@@ -8,12 +8,50 @@ class IntegracionController extends Zend_Controller_Action
     {
         
     }
-    
+        
+   
     public function facebookAction ()
-    {
-    	
+    {    
+        session_start();
+        require 'Facebook/facebook.php';
+        
+		// Create our Application instance (replace this with your appId and secret).
+		$facebook = new Facebook(array(
+		  'appId'  => '124399157694592',
+		  'secret' => '8c0658db1a7873a56c3551a2ee13464b',
+		));
+		
+		// Get User ID
+		$this->view->user = $facebook->getUser();
+		#$user = $facebook->getUser();
+		
+		// We may or may not have this data based on whether the user is logged in.
+		//
+		// If we have a $user id here, it means we know the user is logged into
+		// Facebook, but we don't know if the access token is valid. An access
+		// token is invalid if the user logged out of Facebook.
+		
+		if ($this->view->user) {
+			try {
+				// Proceed knowing you have a logged in user who's authenticated.
+				$user_profile = $facebook->api('/me');
+			} catch (FacebookApiException $e) {
+				$user = null;
+			}
+		}
+		
+		// Login or logout url will be needed depending on current user state.
+		if ($this->view->user) {
+			$logoutUrl = $facebook->getLogoutUrl();
+			$this->view->link = $logoutUrl;
+			$this->view->userprofile = $user_profile;
+		} else {
+			$loginUrl = $facebook->getLoginUrl(array('scope' => 'read_stream,publish_stream'));
+			$this->view->link = $loginUrl;
+		}
+        
     }
-    
+# --------------------------------------------------------------------------------------------    
     public function twitterAction ()
     {
 
@@ -91,7 +129,7 @@ class IntegracionController extends Zend_Controller_Action
         }
         
     }
-    
+# --------------------------------------------------------------------------------------------    
     public function linkedinAction ()
     {
         session_start();
